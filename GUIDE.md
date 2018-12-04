f12fa18a97fa8045eb5e57a14a7a51b4c195c52b
jorgibravo <jorgibravo@gmail.com>
2018-12-04 21:22:56 +0100

    Serving React With Express

---
+++ GUIDE.md
@@ -0,0 +1,6 @@
+In this step we are going to prepare the project folders and install dependencies.
+Let's start with creating a [new react app](https://github.com/facebook/create-react-app):
+
+```
+yarn create react-app ttt-xmas-iot
+```
--- main/index.js
+++ main/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
 const path = require('path');
-const lightServices = require('./services/lightServices.js');
-const swaggerDocument = require('../swagger.json');
 
 const app = express();
 const port = 8080;
@@ -14,20 +11,6 @@ app.use((req, res, next) => {
 });
 //
 app.use(express.static(path.join(__dirname, '../build')));
-//
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-//
-app.get('/lights/mode/:command', (req, res) => {
-  res.send(lightServices.setLightMode(req.params.command));
-});
-//
-app.get('/lights/status', (req, res) => {
-  res.send(lightServices.getLightStatus());
-});
-//
-app.get('/lights/speed/:speed', (req, res) => {
-  res.send(lightServices.setLightSpeed(req.params.speed));
-});
 //
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'));
