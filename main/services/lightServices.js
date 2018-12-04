a6156d4da56d4bbf06dda8dd8fcfbec917340b3e
jorgibravo <jorgibravo@gmail.com>
2018-12-04 23:22:49 +0100

    debug

--- main/services/lightServices.js
+++ main/services/lightServices.js
@@ -67,7 +67,7 @@ const colorwheel = pos => {
 //
 // This handles the loop outside of the intervalFunctions
 const increment = () => {
-  // console.log('DIRECTION:', direction, 'ledId:', ledId);
+  console.log('DIRECTION:', direction, 'ledId:', ledId);
   if (direction === 'FORWARD') {
     if (ledId === NUM_LEDS - 1) {
       if (continousAnimation === true) {
@@ -150,9 +150,8 @@ const playAnimation = type => {
         pixelData[ledId] = color;
         ws281x.render(pixelData);
         if (ledId + 1 < NUM_LEDS) {
+          console.log('Increment');
           increment();
-        } else {
-          clearInterval(activeAnimation);
         }
       }, ledSpeed);
       //
@@ -185,6 +184,7 @@ const setLightMode = type => {
     case 'red':
     case 'green':
       if (animationName !== 'off') {
+        console.log('clear interval from api');
         clearInterval(activeAnimation);
         ws281x.reset();
       }
