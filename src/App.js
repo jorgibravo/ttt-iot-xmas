3220898a1c9d0ae9b3d298abe9116fc8421d3796
Gergely Sipos <gergely.sipos@blackswan.com>
2020-01-10 12:35:04 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -90,10 +90,10 @@ class App extends Component {
                     <option value="scanner">scanner</option>
                     <option value="red">red</option>
                     <option value="green">green</option>
-                    <option value="lepcsofellentrol">lepcsofelfentrol</option>
-                    <option value="lepcsofelfentrol">lepcsofellentrol</option>
-                    <option value="lepcsolelentrol">lepcsolefentrol</option>
-                    <option value="lepcsolefentrol">lepcsolelentrol</option>
+                    <option value="lepcsofellentrol">lepcsofellentrol</option>
+                    <option value="lepcsofelfentrol">lepcsofelfentrol</option>
+                    <option value="lepcsolelentrol">lepcsolelentrol</option>
+                    <option value="lepcsolefentrol">lepcsolefentrol</option>
                   </NativeSelect>
                 </FormControl>
               </CardContent>
